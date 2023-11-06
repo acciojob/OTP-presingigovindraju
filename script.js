dd09449aea490526ea65b10e7075558bef38fd91
presingigovindraju <141930032+presingigovindraju@users.noreply.github.com>
2023-11-06 13:12:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+// Select all OTP input elements
+const otpInputs = document.querySelectorAll(".code");
+
+// Add an event listener to each OTP input for automatically focusing on the next input
+otpInputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const currentInput = e.target;
+    if (currentInput.value.length === 1) {
+      if (index < otpInputs.length - 1) {
+        otpInputs[index + 1].focus();
+      } else {
+        currentInput.blur();
+      }
+    } else if (currentInput.value.length === 0) {
+      if (index > 0) {
+        otpInputs[index - 1].focus();
+      }
+    }
+  });
+
+  // Add an event listener to focus the previous input on backspace key press
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace" && index > 0) {
+      otpInputs[index - 1].focus();
+    }
+  });
+});
